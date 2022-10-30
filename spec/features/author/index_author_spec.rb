c2f05da377c874a1c2e76ec35aa95565daed7f9d
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-10-30 16:32:25 +0100

    Issue 8.1

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -8,15 +8,6 @@ describe "Author index page", :type => :feature do
     it "should have a link to create a new author" do
         visit authors_path
         expect(page).to have_link 'New', href: new_author_path
-    endit "should have a link to the new author page" do
-        visit authors_path
-        expect(page).to have_link('Add author', href: new_author_path)
-    end
-    it "should have a link to each author's edit page" do
-        visit authors_path
-        Author.all.each do |author|
-            expect(page).to have_link('Edit', href: edit_author_path(author))
-        end
     end
     
     it "should have a table with a row for each author" do
