2ebd3d22673a610cbdf452d0db77b9d0fe806287
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-10-31 18:56:59 +0100

    Issue 12

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_161847) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_175406) do
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "homepage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_papers", id: false, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "paper_id", null: false
   end
 
   create_table "papers", force: :cascade do |t|
