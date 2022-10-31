5205deb45f59d02fd70a2492bed6bf326ec39887
Avetis Navasardyan <avetis.navasardyan@student.hpi.uni-potsdam.de>
2022-10-31 20:06:22 +0100

    Issue 15

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,7 +1,8 @@
 class Paper < ApplicationRecord
-
     has_and_belongs_to_many :authors
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: { only_integer: true }
+
+    scope :year, -> (year) { where year: year } 
 end
